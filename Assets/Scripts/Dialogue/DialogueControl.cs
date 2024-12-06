9b96f746a371c4623d61ba63db7b0be20b33ecae
Pedro Vinicius <128930886+kydorpe@users.noreply.github.com>
2024-12-06 12:32:56 -0300

    update dialogo
    introdução do loop de acionamento de dialogo e HUD

---
+++ Assets/Scripts/Dialogue/DialogueControl.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueControl : MonoBehaviour
+{
+    [Header("Components")]
+
+    public GameObject dialogueOBJ; // janela 
+    public Image profileSprite;     // sprite perfil
+    public Text speechText; // texto fala
+    public Text actorNameText; // name NPC
+
+    [Header("Settings")]
+
+    public float typingSpeed; // velocidade da fala
+
+
+    // variaveis de controle
+
+
+    private bool isShowing; // está visivel?
+    private int index; // leitor de quantidade 
+    private string [] sentences;
+
+
+    public static DialogueControl instance;
+
+
+    public void Awake ()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        
+    }
+
+    IEnumerator TypeSentence ()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            speechText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    // salto de frase // fala
+    public void NextSentence()
+    {
+
+    }
+
+    // inicia a fala do NPC
+    public void Speech(string[] txt)
+    {
+        if(!isShowing)
+        {
+            dialogueOBJ.SetActive(true);
+            sentences = txt;
+            StartCoroutine(TypeSentence());
+            isShowing = true;
+        }
+        
+    }
+
+}
---
+++ Assets/Scripts/Dialogue/NPC/NPC_Dialogue.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class NPC_Dialogue : MonoBehaviour
+{
+    public float dialogueRange;
+    public LayerMask playerLayer;
+    public bool playerHit;
+    public DialogueSettings dialogue;
+    private List <string> sentences = new List<string>();
+   
+    void Start()
+    {
+        GetNPCInfo();
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E)&&playerHit)
+        {
+            DialogueControl.instance.Speech(sentences.ToArray());
+        }
+    }
+
+    void GetNPCInfo()
+    {
+        for (int i = 0; i < dialogue.dialogues.Count; i++)
+        {
+            sentences.Add(dialogue.dialogues[i].sentence.portuguese);
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        ShowDialogue();
+    }
+
+
+    void ShowDialogue()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, dialogueRange, playerLayer);
+        if (hit != null)
+        {
+            playerHit = true;
+        }
+        else
+        {
+            playerHit = false;
+            DialogueControl.instance.dialogueOBJ.SetActive(false);
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, dialogueRange);
+    }
+}
